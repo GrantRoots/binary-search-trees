40329ab2e72cc5f2f98b356bc4e4bc268ff62d92
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-11-01 21:19:38 -0700

    Make insert()
    adds items to the leafs of the tree

--- scirpt.js
+++ scirpt.js
@@ -11,89 +11,111 @@ class Node {
 class Tree {
     constructor(array) {
         this.array = array
-        this.root = buildTree()
+        this.root = this.buildTree(array)
     }
-}
 
-function buildTree(array) {
-    if (array.length <= 1) {
-        return new Node(array[0])
+    buildTree(array) {
+        if (array.length <= 1) {
+            return new Node(array[0])
+        }
+        let sortedArray = array.sort(function(a, b) {
+            return a - b
+        })
+        //remove duplicates
+        for (let i = 0; i < sortedArray.length; i++) {
+            let current = sortedArray[i]
+            let previous = sortedArray[i-1]
+            if (current === previous) {
+                sortedArray.splice(i, 1)
+            }
+        }
+    
+        //get middle value of array
+        let middleValue = new Node(sortedArray[Math.floor(sortedArray.length / 2)])
+    
+        //get middle value of left
+        let leftHalf = sortedArray.slice(0, Math.floor(sortedArray.length / 2))
+        //get middle value of right repeat until done
+        let rightHalf = sortedArray.slice(Math.floor(sortedArray.length / 2) + 1, sortedArray.length)
+        //call recursively
+        middleValue.left = this.buildTree(leftHalf)
+        middleValue.right = this.buildTree(rightHalf)
+    
+        return middleValue
     }
-    let sortedArray = array.sort(function(a, b) {
-        return a - b
-    })
-    //remove duplicates
-    for (let i = 0; i < sortedArray.length; i++) {
-        let current = sortedArray[i]
-        let previous = sortedArray[i-1]
-        if (current === previous) {
-            sortedArray.splice(i, 1)
+    
+    insert(value) {
+        //insert duplicates?
+        let currentNode = this.root
+        while (value !== currentNode.data) {
+            //go left
+            if (value < currentNode.data) {
+                if (currentNode.left === null) {
+                    return currentNode.left = new Node(value)
+                }
+                currentNode = currentNode.left
+            }
+            //go right
+            if (value > currentNode.data) {
+                if (currentNode.right === null) {
+                    return currentNode.right = new Node(value)
+                }
+                currentNode = currentNode.right
+            }
         }
     }
-
-    //get middle value of array
-    let middleValue = new Node(sortedArray[Math.floor(sortedArray.length / 2)])
     
-    console.log(sortedArray[Math.floor(sortedArray.length / 2)])
+    deleteItem(value) {
+        //leaf
+        if (value === root) {
+    
+        }
+        if (value < root) {
+    
+        }
+    
+        //one child
+    
+    
+        //two children
+        //replace with next biggest
+    }
+    
+    find(value) {
+    
+    }
+    
+    levelOrder(callback) {
+    
+    }
+    
+    inOrder(callback) {
+    
+    }
+    
+    preOrder(callback) {
+    
+    }
+    
+    postOrder(callback) {
+    
+    }
+    
+    height(node) {
+    
+    }
+    
+    depth(node) {
+    
+    }
+    
+    isBalanced() {
+    
+    }
+    
+    rebalance() {
 
-    //get middle value of left
-    let leftHalf = sortedArray.slice(0, Math.floor(sortedArray.length / 2))
-    //get middle value of right repeat until done
-    let rightHalf = sortedArray.slice(Math.floor(sortedArray.length / 2) + 1, sortedArray.length)
-    //call recursively
-    middleValue.left = buildTree(leftHalf)
-    middleValue.right = buildTree(rightHalf)
-
-    return middleValue
-}
-
-function insert(value) {
-    //goes to leaf
-}
-
-function deleteItem(value) {
-    //leaf
-
-    //one child
-
-    //two children
-    //replace with next biggest
-}
-
-function find(value) {
-
-}
-
-function levelOrder(callback) {
-
-}
-
-function inOrder(callback) {
-
-}
-
-function preOrder(callback) {
-
-}
-
-function postOrder(callback) {
-
-}
-
-function height(node) {
-
-}
-
-function depth(node) {
-
-}
-
-function isBalanced() {
-
-}
-
-function rebalance() {
-
+    }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -109,8 +131,13 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
 };
 
-prettyPrint(buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
-let testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-console.log(testArr.sort(function(a, b) {
-    return a - b
-}))
+//testing
+
+let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+console.log(test.root)
+
+prettyPrint(test.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
+
+test.insert('2')
+console.log(test.root)
+prettyPrint(test.root)
